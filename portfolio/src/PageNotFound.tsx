e5121e2682ba4bf4010c965e7f47cd4ed2e85ea0
BurgerFox1 <jeloaguilar1@gmail.com>
2024-07-22 02:45:58 +0800

    directory fix

--- portfolio/src/PageNotFound.tsx
+++ portfolio/src/PageNotFound.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CSSProperties } from 'react';
 
 const NotFound: React.FC = () => {
   return (
@@ -9,7 +10,7 @@ const NotFound: React.FC = () => {
   );
 };
 
-const styles = {
+const styles: { [key: string]: CSSProperties } = {
   container: {
     display: 'flex',
     flexDirection: 'column',
